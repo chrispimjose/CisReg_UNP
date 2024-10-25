02f1084162c2c2e733bf09f07d8a556e4702c0d7
dudublessed <idicasapple@icloud.com>
2024-10-25 10:55:16 -0300

    Implementar funcionalidade de Login

--- CisReg_Website/Controllers/LoginController.cs
+++ CisReg_Website/Controllers/LoginController.cs
@@ -1,11 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using CisReg_Website.Models;
+using Newtonsoft.Json;
+using CisReg_Website.Data;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace CisReg_Website.Controllers
 {
     public class LoginController : Controller
     {
+
         [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Submit(LoginModel model)
+        {    
+            if (ModelState.IsValid)
+            {
+                var filtro = Builders<BsonDocument>.Filter.And(
+                    Builders<BsonDocument>.Filter.Eq("Email", model.Email),
+                    Builders<BsonDocument>.Filter.Eq("Password", model.Password)
+                    );
+                var result = Database.GetInstance().Select("profissional", filtro);
+
+                if (result != null && result.Count > 0)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Home()
         {
             return View();
         }
